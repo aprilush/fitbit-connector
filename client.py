3925615e0c573d507ecd46fb6e211679df318109
Laura Dragan <aprilush@gmail.com>
2013-08-02 20:45:14 +0100

    calls to all the resource access api

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import argparse, logging, pprint, json
 from fitbit import Fitbit
+from fitbit_timeseries import FitbitTimeseries
+from fitbit_resources import FitbitResources
 
 """ A simple command-line client to demontrate usage of the library. """
 
@@ -15,8 +17,118 @@ if args['debug']:
 else:
     logging.basicConfig(level=logging.INFO)
 
-fitbit = fitbit.Fitbit()
+consumer_key = "c9c3c3541fc14f8e839b5e51d21e7011"
+consumer_secret = "353a8e82be6748dc9d83fba425fe0af5"
+access_token_key = "f42721b1a764eab409991c0419ba58ed"
+access_token_secret = "e2fae176127c712f1e54a14da23b051b"
+
+# fitbit = fitbit.Fitbit(consumer_key, consumer_secret)
+fitbit = Fitbit(consumer_key, consumer_secret, access_token_key, access_token_secret)
 if (fitbit.token == None):
 		fitbit.get_token()
-pprint.pprint(json.loads(fitbit.get_user_info()))
+
+fitbit_resources = FitbitResources(fitbit)
+
+pprint.pprint(fitbit_resources.get_user_info())
+
+pprint.pprint(fitbit_resources.get_body_measurements(date='2012-03-14'))
+
+# pprint.pprint(fitbit_resources.get_body_weight(to_date='2012-03-14'))
+# pprint.pprint(fitbit_resources.get_body_weight(to_date='2012-03-14', from_date='2012-02-17'))
+# pprint.pprint(fitbit_resources.get_body_weight(to_date='2012-03-14', period='1m'))
+
+# pprint.pprint(fitbit_resources.get_body_fat(to_date='2012-03-14'))
+# pprint.pprint(fitbit_resources.get_body_fat(to_date='2012-03-14', from_date='2012-02-17'))
+# pprint.pprint(fitbit_resources.get_body_fat(to_date='2012-03-14', period='1m'))
+
+# pprint.pprint(fitbit_resources.get_body_weight_goal())
+# pprint.pprint(fitbit_resources.get_body_fat_goal())
+
+# pprint.pprint(fitbit_resources.get_activities(date='2012-03-14'))
+# pprint.pprint(fitbit_resources.get_activity_daily_goals())
+# pprint.pprint(fitbit_resources.get_activity_weekly_goals())
+
+# pprint.pprint(fitbit_resources.get_foods(date='2012-03-14'))
+# pprint.pprint(fitbit_resources.get_water(date='2012-03-14'))
+# pprint.pprint(fitbit_resources.get_food_goals())
+
+# pprint.pprint(fitbit_resources.get_sleep(date='2012-03-14'))
+# pprint.pprint(fitbit_resources.get_heart_rate(date='2012-03-14'))
+# pprint.pprint(fitbit_resources.get_blood_pressure(date='2012-03-14'))
+# pprint.pprint(fitbit_resources.get_glucose(date='2012-03-14'))
+
+# get_time_series tested separately, through the helper functions in fitbit_resources_timeseries
+
+# pprint.pprint(fitbit_resources.get_activity_stats())
+# pprint.pprint(fitbit_resources.get_recent_activities())
+# pprint.pprint(fitbit_resources.get_frequent_activities())
+# pprint.pprint(fitbit_resources.get_favorite_activities())
+
+# pprint.pprint(fitbit_resources.get_recent_foods())
+# pprint.pprint(fitbit_resources.get_frequent_foods())
+# pprint.pprint(fitbit_resources.get_favorite_foods())
+# pprint.pprint(fitbit_resources.get_meals())
+
+# pprint.pprint(fitbit_resources.get_friends())
+# pprint.pprint(fitbit_resources.get_friends_leaderboard())
+# pprint.pprint(fitbit_resources.get_invites()) # getting 401 Unauthorized - works in API explorer
+# pprint.pprint(fitbit_resources.get_badges())
+
+# devices = fitbit_resources.get_devices()
+# pprint.pprint(devices)
+# pprint.pprint(fitbit_resources.get_alarms('198353')) # getting 400 Bad request - also no longer available in the API explorer
+
+# activities = fitbit_resources.browse_activities()
+# pprint.pprint(activities)
+# pprint.pprint(fitbit_resources.get_activity(activities['categories'][0]['activities'][0]['id']))
+
+# white_bread = fitbit_resources.search_foods('white bread')
+# pprint.pprint(white_bread)
+# pprint.pprint(fitbit_resources.get_food(white_bread['foods'][0]['foodId']))
+
+# pprint.pprint(fitbit_resources.get_food_units())
+
+
+# fitbit_timeseries = FitbitTimeseries(fitbit)
+
+# pprint.pprint(fitbit_timeseries.get_foods_caloriesIn())
+# pprint.pprint(fitbit_timeseries.get_foods_water())
+
+# pprint.pprint(fitbit_timeseries.get_activities_calories())
+# pprint.pprint(fitbit_timeseries.get_activities_caloriesBMR())
+# pprint.pprint(fitbit_timeseries.get_activities_steps())
+# pprint.pprint(fitbit_timeseries.get_activities_distance())
+# pprint.pprint(fitbit_timeseries.get_activities_floors())
+# pprint.pprint(fitbit_timeseries.get_activities_elevation())
+# pprint.pprint(fitbit_timeseries.get_activities_minutesSedentary())
+# pprint.pprint(fitbit_timeseries.get_activities_minutesLightlyActive())
+# pprint.pprint(fitbit_timeseries.get_activities_minutesFairlyActive())
+# pprint.pprint(fitbit_timeseries.get_activities_minutesVeryActive())
+# pprint.pprint(fitbit_timeseries.get_activities_activeScore())
+# pprint.pprint(fitbit_timeseries.get_activities_activityCalories())
+
+# pprint.pprint(fitbit_timeseries.get_activities_tracker_calories())
+# pprint.pprint(fitbit_timeseries.get_activities_tracker_steps())
+# pprint.pprint(fitbit_timeseries.get_activities_tracker_distance())
+# pprint.pprint(fitbit_timeseries.get_activities_tracker_floors())
+# pprint.pprint(fitbit_timeseries.get_activities_tracker_elevation())
+# pprint.pprint(fitbit_timeseries.get_activities_tracker_minutesSedentary())
+# pprint.pprint(fitbit_timeseries.get_activities_tracker_minutesLightlyActive())
+# pprint.pprint(fitbit_timeseries.get_activities_tracker_minutesFairlyActive())
+# pprint.pprint(fitbit_timeseries.get_activities_tracker_minutesVeryActive())
+# pprint.pprint(fitbit_timeseries.get_activities_tracker_activeScore())
+# pprint.pprint(fitbit_timeseries.get_activities_tracker_activityCalories())
+
+# pprint.pprint(fitbit_timeseries.get_sleep_startTime())
+# pprint.pprint(fitbit_timeseries.get_sleep_timeInBed())
+# pprint.pprint(fitbit_timeseries.get_sleep_minutesAsleep())
+# pprint.pprint(fitbit_timeseries.get_sleep_awakeningsCount())
+# pprint.pprint(fitbit_timeseries.get_sleep_minutesAwake())
+# pprint.pprint(fitbit_timeseries.get_sleep_minutesToFallAsleep())
+# pprint.pprint(fitbit_timeseries.get_sleep_minutesAfterWakeup())
+# pprint.pprint(fitbit_timeseries.get_sleep_efficiency())
+
+# pprint.pprint(fitbit_timeseries.get_body_weight())
+# pprint.pprint(fitbit_timeseries.get_body_bmi())
+# pprint.pprint(fitbit_timeseries.get_body_fat())
 
--- fitbit.py
+++ fitbit.py
@@ -1,19 +1,20 @@
 import json, urllib, urllib2, urlparse, logging, cookielib, pprint
+from datetime import date
 import oauth2 as oauth
 
+# TODO add an accept-language header to set the measurement units to us or metric
 class Fitbit:
       
-    def __init__(self, access_token_key=None, access_token_secret=None):
-        self.logger = logging.getLogger("python-fitbit-oauth")
+    def __init__(self, consumer_key, consumer_secret, access_token_key=None, access_token_secret=None):
+        self.logger = logging.getLogger("fitbit")
 
         self.api_base_url = "http://api.fitbit.com"
+
+        self.consumer = oauth.Consumer(consumer_key, consumer_secret)
+
         self.token = None
         if (access_token_key !=None and access_token_secret !=None):
-        	self.token = oauth.Token(access_token_key, access_token_secret)
-
-        consumer_key = "c9c3c3541fc14f8e839b5e51d21e7011"
-        consumer_secret = "353a8e82be6748dc9d83fba425fe0af5"
-        self.consumer = oauth.Consumer(consumer_key, consumer_secret)
+            self.token = oauth.Token(access_token_key, access_token_secret)
 
     def get_token(self):
         baseurl = "https://www.fitbit.com"
@@ -50,8 +51,7 @@ class Fitbit:
         self.logger.info("Successfully got access oauth_token: {0}, oauth_token_secret: {1}".format(self.token['oauth_token'], self.token['oauth_token_secret']))
         return self.token
 
-    def get_user_info(self, format='json'):
-        url = "{0}/1/user/-/profile.{1}".format(self.api_base_url, format)
+    def call_get_api(self, url):
         req = oauth.Request.from_consumer_and_token(consumer=self.consumer, token=self.token, http_method="GET", http_url=url)
         sig_method = oauth.SignatureMethod_HMAC_SHA1()
         req.sign_request(sig_method, self.consumer, self.token)
@@ -64,17 +64,15 @@ class Fitbit:
         data = resp.read()
         return data
 
-    def get_food_units(self, format='json'):
-        url = "{0}/1/foods/units.{1}".format(self.api_base_url, format)
-        req = oauth.Request.from_consumer_and_token(consumer=self.consumer, token=self.token, http_method="GET", http_url=url)
-        sig_method = oauth.SignatureMethod_HMAC_SHA1()
-        req.sign_request(sig_method, self.consumer, self.token)
-        headers = req.to_header()
-
-        req = urllib2.Request(url)
-        req.get_method = lambda: "GET"
-        req.add_header('Authorization', headers['Authorization'])
-        resp = urllib2.urlopen(req)
-        data = resp.read()
-        return data
-
+    def get_time_series(self, resource_path, to_date=date.today().isoformat(), from_date=None, period='max', format='json'):
+        # from_date and to_date in the format yyyy-MM-dd
+        # period can be one of {1d, 7d, 30d, 1w, 1m, 3m, 6m, 1y, max}
+        # to_date takes precedence over period, if both are given
+        # use fitbit_timeseries helper functions for a list of possible resource paths
+        if (from_date == None):
+            url = "{0}/1/user/-/{1}/date/{2}/{3}.{4}".format(self.api_base_url, resource_path, to_date, period, format)
+        else:
+            url = "{0}/1/user/-/{1}/date/{2}/{3}.{4}".format(self.api_base_url, resource_path, from_date, to_date, format)
+        data = self.call_get_api(url)
+        return json.loads(data)
+        return
